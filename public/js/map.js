9c2728818995b58a03bdfb370cefabd5c0d1d735
Trisha-05 <trishapavani@gmail.com>
2024-12-28 20:21:58 +0530

    Add project files

---
+++ public/js/map.js
@@ -0,0 +1,8 @@
+   
+    mapboxgl.accessToken=mapToken;
+    const map=new mapboxgl.Map({
+        container:"map",
+        style:"mapbox://styles/mapbox/streets-v12",
+        center:[77.209,28.639],
+        zoom:9,
+    });
---
+++ routes/listing.js
@@ -0,0 +1,48 @@
+const express=require("express");
+const router=express.Router();
+const  wrapAsync=require("../utils/wrapAsync.js");
+const ExpressError=require("../utils/ExpreeError.js");
+const {listingSchema}=require("../schema.js");
+const Listing = require("../models/listing.js");
+const Review = require("../models/review.js");
+const {isLoggedIn,isOwner,validateListing}=require("../middleware.js");
+
+const listingController = require("../controllers/listings.js");
+const multer  = require('multer');
+const {storage}= require("../cloudConfig.js");
+const upload = multer({ storage });
+
+router.get("/filter/:id",wrapAsync(listingController.filter));
+
+router
+.route("/")
+.get(wrapAsync(listingController.index)) //index route
+ .post(
+    isLoggedIn,
+    upload.single('listing[image]'),
+    validateListing,
+ wrapAsync(listingController.createListing));//create route
+
+
+
+//new route
+router.get("/new",isLoggedIn,listingController.renderNewForm)
+ 
+router
+.route("/:id")
+.get(wrapAsync(listingController.showListing)) //show route
+.put(isLoggedIn,
+    isOwner,
+    upload.single('listing[image]'),
+    validateListing,
+    wrapAsync( listingController.updateListing))//update listing
+.delete(isLoggedIn,
+    isOwner,
+    wrapAsync(listingController.destroyListing));// delete route
+ 
+ 
+ //edit route
+ router.get("/:id/edit", isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm));
+ 
+ 
+module.exports = router;
